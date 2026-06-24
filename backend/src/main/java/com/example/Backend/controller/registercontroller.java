670975e2f5c381755c2ea34d25cc3b0219d229d6
sire-k <ash.yakkha@gmail.com>
2026-06-24 10:28:22 +0545

    Changes to registercontroller

--- backend/src/main/java/com/example/Backend/controller/registercontroller.java
+++ backend/src/main/java/com/example/Backend/controller/registercontroller.java
@@ -2,12 +2,15 @@ package com.example.Backend.controller;
 
 import com.example.Backend.model.SUser;
 import com.example.Backend.repository.userRepository;
+import com.example.Backend.DTO.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class registercontroller {
     @Autowired
@@ -23,7 +26,22 @@ public class registercontroller {
 
         return ResponseEntity.ok().build();
     }
+@PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        // TO Find the user via email
+        Optional<SUser> userOptional = userRepo.findByEmail(loginRequest.getEmail());
 
+        if (userOptional.isPresent()) {
+            SUser user = userOptional.get();
+            
+            if (passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                return ResponseEntity.ok("{\"message\": \"Login successful!\"}");
+            }
+        }
+        
+        // Error Handler
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"error\": \"Invalid email or password\"}");
+    }
     @GetMapping("/home")
     public String yes(){return "registered";}
 }
