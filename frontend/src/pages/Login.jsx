4e93728e39b55db35897f4f7388a1aeac6966e1d
sire-k <ash.yakkha@gmail.com>
2026-06-24 10:19:36 +0545

    Changes to LogIn as contribution

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,70 @@
+import { useState } from "react";
 import "../style/Login.css";
 
 const Login = () => {
+  // For Tracking Inputs
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Connection with back-end
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault(); // To deug and avoid refreshing
+    setErrorMessage(""); 
+
+    try {
+      // Sending request controller
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+          email: email, 
+          password: password 
+        }), 
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Success:", data.message);
+        
+        // Routing to dashboard/homepage upon success
+        window.location.href = "/home"; 
+      } else {
+        setErrorMessage("Invalid email or password.");
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+      setErrorMessage("Could not connect to the server.");
+    }
+  };
+
   return (
     <div className="login-body">
       <div className="login-container">
         <h1>Welcome Back</h1>
         <p className="subtitle">Sign in to continue</p>
 
-        <form className="login-form">
+        {errorMessage && <p style={{ color: "red", textAlign: "center" }}>{errorMessage}</p>}
+
+        <form className="login-form" onSubmit={handleLoginSubmit}>
           <input
             type="email"
             placeholder="Email Address"
             className="login-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
 
           <input
             type="password"
             placeholder="Password"
             className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
           />
 
           <button type="submit" className="login-btn">
